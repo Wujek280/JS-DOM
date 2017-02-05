49a5b793af17c67bd2ec99ec39bcb7dbf68e337c
Wujek <mjwojcik94@gmail.com>
2017-02-05 13:05:03 +0100

    SPRAWDZANIE CZY HTML ELEMENT

--- js/3-nodes.js
+++ js/3-nodes.js
@@ -2,7 +2,29 @@
 
 var pierwszyDiv = document.getElementById('parFirst');
 
-console.log(pierwszyDiv.parentNode);
+//console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.childNodes); //wszystko
+console.log(pierwszyDiv.children[0]);  // tylko HTML
+
+//children - otrzymuje tablice ogarniętą
+//child - otrzymuje obiekt ( w chuj tam komentarzy i tekstow )
+
+var pierwszyChild = pierwszyDiv.childNodes[0];
+   console.log(pierwszyDiv.pierwszyChild);
+   console.log(pierwszyDiv.previousSibling.previousSibling);
+   console.log(pierwszyDiv.nextSibling);
+   console.log(pierwszyDiv.nextSibling.nextSibling);
+
+
+var childNodesDiva = pierwszyDiv.childNodes;
+
+childNodesDiva.forEach( function(element) {
+   if(element.nodeType == 1) {
+      console.log("TAK >> HTML "+element);
+   } else { 
+      console.log("NIE >> HTML "+element);
+   }
+});
 
 
 
