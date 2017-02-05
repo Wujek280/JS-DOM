c89f032c21b6ace67ecf40e049d6f52ab8a8ff56
Wujek <mjwojcik94@gmail.com>
2017-02-05 15:10:05 +0100

    stop propagation

--- js/6-events.js
+++ js/6-events.js
@@ -7,7 +7,7 @@ var secondLink = document.getElementsByTagName("a")[1];
 
       e.preventDefault();
       console.log('Czy masz 18 lat?');
-      console.log(e.clientX);
+      console.log(e.clientX+" "+ e.clientY );
    }
 
    secondLink.onclick = alarm;
@@ -19,6 +19,30 @@ var thirdLink = document.getElementsByTagName("a")[2];
 thirdLink.addEventListener('click', alarm);
 
 
+var mainHeader = document.getElementById('main-header');
 
+function resize(e) {
+   if(e.type == 'mouseover'){
+      //mainHeader.style.fontSize = "40px";
+   } else {
+      //mainHeader.style.fontSize = "inherit";
+   }
+}
+
+mainHeader.onmouseover = resize; 
+mainHeader.onmouseout = resize; 
+
+var header = document.getElementsByTagName('header')[0].onclick = klikHeader;
+
+var header = document.getElementsByTagName('h1')[0].onclick = klikH1;
+
+   function klikHeader(e){
+      console.log(" kliknales w header ");
+   }
+
+   function klikH1(e){
+      e.stopPropagation();
+      console.log(" kliknales w h1 ");
+   }
 
 
