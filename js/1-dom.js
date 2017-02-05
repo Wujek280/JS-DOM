f975b7eaff001d247554caf67dffc3593525aca3
Wujek <mjwojcik94@gmail.com>
2017-02-05 11:42:25 +0100

    pierwsze selektory

---
+++ js/1-dom.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log (window);
+console.log (window.innerHeight);
+
+console.log (document.head);
+console.log (document.body);
+console.log (document.documentElement);
+
+console.log (document.innerHeight);
+console.log (document.outerHeight);
---
+++ js/2-search-element.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+var parFirst = document.getElementById('parFirst');
+
+var linki = document.getElementsByClassName('link');
+
+var linkiPoTagu = document.getElementsByTagName('a');
+
+var linkiPoSelektorze = document.querySelectorAll('.link');
+   console.log(linkiPoSelektorze);
+
